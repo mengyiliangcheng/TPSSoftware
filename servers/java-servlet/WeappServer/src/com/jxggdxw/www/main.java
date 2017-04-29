bb583bb32a436258f55d82145043f929085d67b1
mengyiliangcheng <mengyiliangcheng@gmail.com>
2017-04-29 18:46:46 +0800

    创建一个eclipse工程
    -add: 显示一个helloworld网页，以后将基于这个工程进行开发

---
+++ servers/java-servlet/WeappServer/src/com/jxggdxw/www/main.java
@@ -0,0 +1,68 @@
+package com.jxggdxw.www;
+
+import java.io.IOException;  
+import java.io.PrintWriter;  
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+
+//@WebServlet(name="MainServlet")
+
+public class main extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+    //private static Logger logger = LoggerFactory.getLogger(FileUploadServlet.class);
+
+  
+    public void doGet(HttpServletRequest request, HttpServletResponse response)  
+            throws IOException, ServletException { 
+    	
+        PrintWriter out = response.getWriter();  
+        out.write("<html>\r\n");  
+        out.write("<head>\r\n");  
+        // 设定解码方式  
+        out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n");  
+        out.write("</head>\r\n");  
+        out.write("\r\n");  
+        out.write("<body>\r\n");  
+        out.write("<H1>\r\n");  
+        out.write("helloworld");  
+        out.write("\r\n");  
+        out.write("</H1>\r\n");  
+        out.write("</body>\r\n");  
+        out.write("</html>");  
+        /*
+    	String info = request.getParameter("info");
+    	PrintWriter out = response.getWriter();
+    	out.write("<html>");
+    	out.write("<head><title>TITLE</title></head>");
+    	out.write("<body>");
+    	out.write("<h1>"+info+"</h1>");
+    	out.write("</body>");
+    	out.write("</html>");
+    	out.close();
+    	System.out.println("get");
+    	*/
+    }  
+    
+    public void doPost(HttpServletRequest request, HttpServletResponse response)  
+            throws IOException, ServletException
+    {
+    	System.out.println("post");
+    	this.doGet(request, response);
+    }
+    
+    public void init() throws ServletException
+    {
+    	System.out.println("init..");
+    }
+    
+    public void destory()
+    {
+    	System.out.println("destory..");
+    	
+    }
+
+}
