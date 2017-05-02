947a1f4b3bb8a00d1ea1e5951873292e5143b6ff
1993TB <623278193@qq.com>
2017-05-02 21:09:30 +0800

    master
    规范化程序，增加多个图片依次上传功能

--- weapp/pages/third/third.js
+++ weapp/pages/third/third.js
@@ -1,5 +1,5 @@
 // pages/third/third.js
-var uploadfn = require('../../utils/conf.js')
+var uploadfn = require('../../utils/upload.js')
 var app = getApp()
 const ctx = wx.createCanvasContext('myCanvas')
 var initData = 'this is first line\nthis is second line'
@@ -7,73 +7,36 @@ var extraLine = []
 var tempFilePaths
 var newgoodsname
 var goodsname
+var newgoodsprice
+var newgoodsabstract
 Page({
   data:{},
   newgoodsname:function(e){
     newgoodsname = e.detail.value
-    //console.log(newgoodsname)
+    console.log(newgoodsname)
 
   },
   newgoodsprice:function(e){
-    var newgoodsprice = e.detail.value
+    newgoodsprice = e.detail.value
     console.log(newgoodsprice)
   },
   newgoodsabstract:function(e){
-    var newgoodsabstract = e.detail.value
+    newgoodsabstract = e.detail.value
     console.log(newgoodsabstract)
   },
 addpicture:function(){
- 
     wx.chooseImage({
-    count: 1, // 默认9
+    count: 9, // 默认9
     sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
     sourceType: ['album'], // 可以指定来源是相册还是相机，默认二者都有
        
     success: function (res) {
     // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-      console.log(res.tempFilePaths[0])
+      console.log(res.tempFilePaths.length)
       var filname='picture'
-      var filepath= res.tempFilePaths[0] 
-       //console.log(tempFilePaths)
-      console.log(filname)
-      //uploadfn(filepath,filname)
-      wx.uploadFile({
-        url:                              "https://32906079.jxggdxw.com:8443/WeappServer/main",  //开发者服务器URL
-        filePath: filepath,  //上传文件的临时路径
-        header: {           //http头
-           'content-type':'application/json' 
-              },
-        name: newgoodsname,    //上传文件所对应的key
-       
-        formData: {           //除了上传文件以外，其他的上传信息
-           op: 'upload',
-           goodsname:newgoodsname
-          },
-        
-        success: function(uploadRes){
-           var data = uploadRes.data
-
-           console.log(goodsname)
-           console.log(data)
-           console.log('uploadRes', uploadRes)
-                    //do something
-            wx.showLoading({
-                title: '上传成功',
-               })
-            setTimeout(function(){
-                 wx.hideLoading()
-                 },1000)
-                 },
-            fail: function(e) {
-                console.log('e', e)
-                wx.showLoading({
-                  title: '上传失败',
-                  })
-             setTimeout(function(){
-                 wx.hideLoading()
-                   },1000)
-                }
-            }) 
+      for(var i=0;i<res.tempFilePaths.length;i++){
+         uploadfn(newgoodsname,newgoodsprice,newgoodsabstract, res.tempFilePaths[i])   //调用图片上传函数
+            }
           }
       })
     }
---
+++ weapp/utils/upload.js
@@ -0,0 +1,42 @@
+//图片上传函数
+var uploadurl='https://32906079.jxggdxw.com:8443/WeappServer/main'
+
+function upload (Goodsname,Goodsprice,Goodsabstract,localfilepath){
+   wx.uploadFile({
+     url:uploadurl,  //开发者服务器URL
+     filePath: localfilepath,  //上传文件的临时路径
+     header: {           //http头
+        'content-type':'application/json' 
+        },
+    name: Goodsname,    //上传文件所对应的key
+    formData: {           //除了上传文件以外，其他的上传信息
+        test: 'upload',
+        goodsname:Goodsname,
+        goodsprice:Goodsprice,
+        goodsabstract:Goodsabstract
+        },
+    success: function(uploadRes){
+        var data = uploadRes.data
+        //console.log(goodsname)
+        console.log(data)
+        console.log('uploadRes', uploadRes)
+        //do something
+        wx.showLoading({
+          title: '上传成功',
+        })
+        setTimeout(function(){
+          wx.hideLoading()
+            },1000)
+        },
+    fail: function(e) {
+        console.log('e', e)
+        wx.showLoading({
+           title: '上传失败',
+        })
+        setTimeout(function(){
+            wx.hideLoading()
+             },1000)
+        }
+    })
+}
+module.exports = upload
