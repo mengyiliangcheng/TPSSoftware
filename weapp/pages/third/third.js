a8edf5e1ecbb0229823aba722da95ddaae5bfd2e
1993TB <623278193@qq.com>
2017-05-03 16:07:25 +0800

    master
    添加同时上传多张图片以及商品信息的功能

--- weapp/pages/third/third.js
+++ weapp/pages/third/third.js
@@ -9,12 +9,12 @@ var newgoodsname
 var goodsname
 var newgoodsprice
 var newgoodsabstract
+var uploadurl='https://32906079.jxggdxw.com:8443/WeappServer/main'
 Page({
   data:{},
   newgoodsname:function(e){
     newgoodsname = e.detail.value
     console.log(newgoodsname)
-
   },
   newgoodsprice:function(e){
     newgoodsprice = e.detail.value
@@ -33,10 +33,42 @@ addpicture:function(){
     success: function (res) {
     // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
       console.log(res.tempFilePaths.length)
-      var filname='picture'
-      for(var i=0;i<res.tempFilePaths.length;i++){
-         uploadfn(newgoodsname,newgoodsprice,newgoodsabstract, res.tempFilePaths[i])   //调用图片上传函数
+      wx.showLoading({
+          title: '上传中',
+        })
+      wx.uploadFile({    //上传第一张图片的同时上传图片信息
+         url:uploadurl,  //开发者服务器URL
+         filePath: res.tempFilePaths[0],  //上传文件的临时路径
+         header: {           //http头
+            'content-type':'application/json' 
+         },
+         formData: {           //除了上传文件以外，其他的上传信息
+            goodsname:newgoodsname,
+            goodsprice:newgoodsprice,
+            goodsabstract:newgoodsabstract,
+            picturename:newgoodsname+'-'+'0'  //第一张图片名称
+          },
+           name: newgoodsname,    //上传文件所对应的key
+         success: function(uploadRes){
+            var data = uploadRes.data
+            console.log('first_upload_success')
+        //do something
+             for(var i=1;i<res.tempFilePaths.length;i++){
+                 var Picturename=newgoodsname+'-'+i  //余后几张图片名称
+                 uploadfn(newgoodsname,Picturename, res.tempFilePaths[i])   //调用图片上传函数  
             }
+         },
+         fail: function(e) {
+            console.log('e', e)
+             wx.showLoading({
+               title: '上传失败',
+          })
+            setTimeout(function(){
+                wx.hideLoading()
+               },1000)
+        }
+    })
+      
           }
       })
     }
